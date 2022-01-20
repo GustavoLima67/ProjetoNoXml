405ec9e288e13896dfa24a3e1772a596c10a7a71
Gustavo <lgusta333@gmail.com>
2022-01-20 15:57:34 -0300

    Initial commit

---
+++ Library/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Employee
+    {
+        private String name;
+        private DateTime birthDate;
+        private Double salary;
+
+        public Employee() { }
+
+        public Employee(String name, DateTime birthDate,Double salary)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.salary = salary;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public DateTime getBirthDate()
+        {
+            return birthDate;
+        }
+
+        public void setBirthDate(DateTime birthDate)
+        {
+            this.birthDate = birthDate;
+        }
+
+        public Double getSalary()
+        {
+            return salary;
+        }
+
+        public void setSalary(Double salary)
+        {
+            this.salary = salary;
+        }
+    }
+}
---
+++ application/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> products = new List<Employee>();
+
+            Console.Write("Enter with name employee: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter with birth date: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter with salary employee: ");
+            double price = double.Parse(Console.ReadLine());
+            
+            Employee pp = new Employee(name, birthDate, price);
+            products.Add(pp);
+
+            XmlSerializer xmlSrl = new XmlSerializer(typeof(Employee));
+
+            StreamWriter stream = new StreamWriter(@"C:\Users\Gustavo\Documents\Arquivos c#\application.xml");
+
+            xmlSrl.Serialize(stream, pp);
+
+            foreach (var e in products)
+            {
+                Console.WriteLine("Employee name: {0}, salary: {1}, birth date: {2}", e.getName(), e.getSalary(), e.getBirthDate());
+            }
+        }
+    }
+}
